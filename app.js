5818a4f042e3e344b3f14161b47ed413877f0e3f
Ashok Nayak <ashoknayak@ashoks-mbp.attlocal.net>
2022-02-20 23:47:09 -0500

    initial template work

---
+++ app.js
@@ -0,0 +1,27 @@
+const generateReadMe = () => {
+    return `
+    
+# Project Title    
+${Project}
+
+## Description
+${Description}
+
+## Programming Languages
+${Languages}
+
+## Installation
+${Installation}
+
+## Tasks Accomplished
+${Tasks}
+
+
+### URL of the deployed application (GitHub Pages):
+<a href="https://${github}.github.io/${repo}
+
+
+### URL of the GitHub repository:
+<a href="https://github.com/${github}/${repo}.git>
+    `
+}
